e5a5ec3797b779b554dd33e1b23e16145f9072ba
lucabertocop <65337262+lucabertocop@users.noreply.github.com>
2024-03-09 02:11:18 +0100

    up

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
-
 let generating = false; // Flag to indicate if generating image
-//attirubutes
+// Attributes
 let yoff = 0.0; // 2nd dimension of Perlin noise
 let echoCount = 0; // Number of echoes
 let echoSpacing = 2; // Spacing between echoes
@@ -11,8 +10,7 @@ let wavec;
 let have_echos;
 let is_glitch;
 
-
-let backgroundColors = ['#00FFFF','#008080','#FFA500','#ADFF2F','#9370DB','#CC0033','#555555','#edebeb']; // Choose background colors
+let backgroundColors = ['#00FFFF', '#008080', '#FFA500', '#ADFF2F', '#9370DB', '#CC0033', '#555555', '#edebeb']; // Choose background colors
 let waveColors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF6600', '#9900FF', '#00CCFF', '#FF0066']; // Choose wave colors
 
 let backgroundColorIndex = 0; // Index to select background color
@@ -36,89 +34,73 @@ function setup() {
   // Create canvas but hide it initially
   canvas = createCanvas(710, 400);
   canvas.hide();
-
 }
 
-// used for looping
+// Used for looping
 function draw() {
   if (generating) {
     generateFromInput();
   }
 }
 
-
-
-
 function generateFromInput() {
   let blockNumber = int(blockNumberInput.value());
   let row = table.findRow(String(blockNumber), 'number');
   if (row) {
-
     let weight = row.get('weight');
     let fee = row.get('fee_reward_btc');
     let nonce = (row.get('nonce'));
     let txs = parseInt(row.get('total_transactions'));
 
-    if (txs >= 1 && txs <= 100) {enableGlitch = true}
-    else is_glitch=false
-    //background, should need to recode this shit, but it works so it's fine
-    if (nonce >= 226570 && nonce <= 537057658) {backgroundColorIndex=7}
-    else if (nonce> 537057658 && nonce <= 1073888746) {backgroundColorIndex=6}
-    else if (nonce>= 1073888747 && nonce <= 1610719836) {backgroundColorIndex=5}
-    else if (nonce>= 1610719837 && nonce <= 2147550925) {backgroundColorIndex=4}
-    else if (nonce>= 2147550926 && nonce <= 2684382014) {backgroundColorIndex=3}
-    else if (nonce>= 2684382015 && nonce <= 3221213103) {backgroundColorIndex=2}
-    else if (nonce>= 3221213104 && nonce <= 3758044192) {backgroundColorIndex=1}
-    else if (nonce>= 3758044193 && nonce <= 4294875281) {backgroundColorIndex=0}
-    // line color
-    if (fee >= 0 && fee <= 10817082) {waveColorIndex=7}
-    else if (fee> 10817083 && fee <= 21634164) {wavewaveColorIndexColors=6}
-    else if (fee>= 21634164 && fee <= 32451246) {waveColorIndex=5}
-    else if (fee>= 32451247 && fee <= 43268328) {waveColorIndex=4}
-    else if (fee>= 43268329 && fee <= 54085410) {waveColorIndex=3}
-    else if (fee>= 54085411 && fee <= 64902492) {waveColorIndex=2}
-    else if (fee>= 64902493 && fee <= 75719574) {waveColorIndex=1}
-    else if (fee>= 75719574 && fee <= 86536657) {waveColorIndex=0}
-    // echo
-    if (txs >= 0 && txs <= 2000) {enableEcho=true}
-    if (txs >= 0 && txs <= 400) {echoCount=6}
-    else if (txs> 400 && txs <= 800) {echoCount=5}
-    else if (txs>= 801 && txs <= 1500) {echoCount=4}
-    else if (txs>= 1501 && txs <= 2000) {echoCount=3}
-  
+    // Toggle glitch effect based on transaction count
+    enableGlitch = txs >= 1 && txs <= 100;
 
+    // Background color
+    backgroundColorIndex = calculateBackgroundColorIndex(nonce);
 
-  background(backgroundColors[backgroundColorIndex]);
-  backgroundc =backgroundColors[backgroundColorIndex];
-  stroke(waveColors[waveColorIndex]);
-  wavec=waveColors[waveColorIndex]
-  noFill();
+    // Wave color
+    waveColorIndex = calculateWaveColorIndex(fee);
 
-  let amplitude = 100; // Controls the amplitude of the wave
-
-  if (enableGlitch) {
-    is_glitch = true
-    applyGlitch(1); // Apply glitch effect with a probability of 10%
-  }
-
-  // Draw the main wave
-  drawWave(0, amplitude);
-
-  // Draw echoes if enabled
-  if (enableEcho) {
-    have_echos = true
-    for (let i = 1; i <= echoCount; i++) {
-      let echoAmplitude = amplitude * 1 / i; // Decrease amplitude for each echo
-      stroke(waveColors[waveColorIndex]);
-      drawWave(i * echoSpacing, echoAmplitude);
+    // Echo
+    enableEcho = txs >= 0 && txs <= 2000;
+    if (enableEcho) {
+      echoCount = calculateEchoCount(txs);
     }
+
+    background(backgroundColors[backgroundColorIndex]);
+    backgroundc = backgroundColors[backgroundColorIndex];
+    stroke(waveColors[waveColorIndex]);
+    wavec = waveColors[waveColorIndex];
+    noFill();
+
+    let amplitude = 100; // Controls the amplitude of the wave
+
+    // Apply glitch effect
+    if (enableGlitch) {
+      is_glitch = true;
+      applyGlitch(0.2, 3);; // Apply glitch effect with a probability of 10%
+    }
+
+    // Draw the main wave
+    drawWave(0, amplitude);
+
+    // Draw echoes if enabled
+    if (enableEcho) {
+      have_echos = true;
+      for (let i = 1; i <= echoCount; i++) {
+        let echoAmplitude = amplitude * 1 / i; // Decrease amplitude for each echo
+        stroke(waveColors[waveColorIndex]);
+        drawWave(i * echoSpacing, echoAmplitude);
+      }
+    }
+
+    let containerDiv = document.getElementById('generatedImageContainer');
+    containerDiv.innerHTML = ''; // Clear previous content
+    containerDiv.appendChild(canvas.elt);
+    canvas.show();
+    updateStats();
   }
-  let containerDiv = document.getElementById('generatedImageContainer');
-  containerDiv.innerHTML = ''; // Clear previous content
-  containerDiv.appendChild(canvas.elt);
-  canvas.show();
-  updateStats();
-}}
+}
 
 function drawWave(phaseOffset, amplitude) {
   beginShape();
@@ -141,14 +123,24 @@ function drawWave(phaseOffset, amplitude) {
   endShape();
 }
 
-function applyGlitch(probability) {
+function applyGlitch(probability, blockSize) {
   loadPixels();
-  for (let i = 0; i < pixels.length; i += 4) {
-    if (random(1) < probability) {
-      // Modify the color channels randomly
-      pixels[i] = int(random(255)); // Red
-      pixels[i + 1] = int(random(255)); // Green
-      pixels[i + 2] = int(random(255)); // Blue
+  for (let x = 0; x < width; x += blockSize) {
+    for (let y = 0; y < height; y += blockSize) {
+      if (random(1) < probability) {
+        let r = int(random(255)); // Red
+        let g = int(random(255)); // Green
+        let b = int(random(255)); // Blue
+        // Modify block of pixels
+        for (let i = x; i < x + blockSize && i < width; i++) {
+          for (let j = y; j < y + blockSize && j < height; j++) {
+            let index = (i + j * width) * 4;
+            pixels[index] = r;
+            pixels[index + 1] = g;
+            pixels[index + 2] = b;
+          }
+        }
+      }
     }
   }
   updatePixels();
@@ -156,25 +148,75 @@ function applyGlitch(probability) {
 
 
 function updateStats() {
-  // Assuming waterTiles, earthTiles, treeTiles, oreTiles, and goldTiles are already defined
   let statsHTML = `
     <p><b>Background color:</b> ${backgroundc}</p>
     <p><b>Wave color:</b> ${wavec}</p>
-    <p><b>is_glitch:</b> ${is_glitch}</p>
-    <p><b>has_echo:</b> ${have_echos}</p>
-    <p><b>echo_count:</b> ${echoCount}</p>
+    <p><b>Is glitch:</b> ${is_glitch}</p>
+    <p><b>Has echo:</b> ${have_echos}</p>
+    <p><b>Echo count:</b> ${echoCount}</p>
   `;
-  // Insert the stats into the 'statsContainer' div
   document.getElementById('statsContainer').innerHTML = statsHTML;
 }
 
+function calculateBackgroundColorIndex(nonce) {
+  if (nonce >= 226570 && nonce <= 537057658) {
+    return 7;
+  } else if (nonce > 537057658 && nonce <= 1073888746) {
+    return 6;
+  } else if (nonce >= 1073888747 && nonce <= 1610719836) {
+    return 5;
+  } else if (nonce >= 1610719837 && nonce <= 2147550925) {
+    return 4;
+  } else if (nonce >= 2147550926 && nonce <= 2684382014) {
+    return 3;
+  } else if (nonce >= 2684382015 && nonce <= 3221213103) {
+    return 2;
+  } else if (nonce >= 3221213104 && nonce <= 3758044192) {
+    return 1;
+  } else if (nonce >= 3758044193 && nonce <= 4294875281) {
+    return 0;
+  }
+}
+
+function calculateWaveColorIndex(fee) {
+  if (fee >= 0 && fee <= 10817082) {
+    return 7;
+  } else if (fee > 10817083 && fee <= 21634164) {
+    return 6;
+  } else if (fee >= 21634164 && fee <= 32451246) {
+    return 5;
+  } else if (fee >= 32451247 && fee <= 43268328) {
+    return 4;
+  } else if (fee >= 43268329 && fee <= 54085410) {
+    return 3;
+  } else if (fee >= 54085411 && fee <= 64902492) {
+    return 2;
+  } else if (fee >= 64902493 && fee <= 75719574) {
+    return 1;
+  } else if (fee >= 75719574 && fee <= 86536657) {
+    return 0;
+  }
+}
+
+function calculateEchoCount(txs) {
+  if (txs >= 0 && txs <= 400) {
+    return 6;
+  } else if (txs > 400 && txs <= 800) {
+    return 5;
+  } else if (txs >= 801 && txs <= 1500) {
+    return 4;
+  } else if (txs >= 1501 && txs <= 2000) {
+    return 3;
+  }
+}
 
 // Add event listener for the "Generate" button
-document.getElementById("generateButton").addEventListener("click", function() {
+document.getElementById("generateButton").addEventListener("click", function () {
   generating = true;
   // Scroll to the stats container
   document.getElementById('statsContainer').scrollIntoView({ behavior: 'smooth' });
 });
+
 // Add event listener for the "Save Image" button
 document.getElementById("saveButton").addEventListener("click", saveImage);
 
